c967d3492a8ad00296b4625cf59c571ccca2a933
duykhangtong <khang.tong1124@hcmut.edu.vn>
2025-03-28 12:10:07 +0700

    UI: contact-page

--- src/components/home_page/HomePage.jsx
+++ src/components/home_page/HomePage.jsx
@@ -23,8 +23,8 @@ export default function HomePage() {
 		speed: 500,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		autoplay: false,
-		autoplaySpeed: 3000,
+		autoplay: true,
+		autoplaySpeed: 2000,
 	};
 
 	const list_slide = [
