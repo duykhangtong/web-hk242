0d602c644cd120ddce116e7c5cf212fa70c82535
Brozic2908 <166984180+Brozic2908@users.noreply.github.com>
2025-04-25 11:27:41 +0700

    new ui

--- backend/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config/config.php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'mk_web');
-define('BASE_URL', 'http://localhost/mk-website/backend');
-define('JWT_SECRET', 'KhangLaDanEmCuaKhanh');
